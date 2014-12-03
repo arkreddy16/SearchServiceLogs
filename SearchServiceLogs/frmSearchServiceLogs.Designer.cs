5365cf94a0c4f7ce0dabcb9b6225b7ce5668f8b9
Ramakrishna <arkreddy16@gmail.com>
2014-12-03 14:16:09 +0530

    Initial Commit

---
+++ SearchServiceLogs/frmSearchServiceLogs.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace SearchServiceLogs
+{
+    partial class frmSearchServiceLogs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.dgvSearchResults = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSetOutputFilePath = new System.Windows.Forms.Button();
+            this.chkExportResults = new System.Windows.Forms.CheckBox();
+            this.lblExportDirectoryLabel = new System.Windows.Forms.Label();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.cbDeviceType = new System.Windows.Forms.ComboBox();
+            this.lblDirectoryPath = new System.Windows.Forms.Label();
+            this.lbloutputDirectoryPath = new System.Windows.Forms.Label();
+            this.btnSetDirectoryPath = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchResults)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device Type :-";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(349, 171);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(99, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Start Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExport);
+            this.groupBox1.Controls.Add(this.dgvSearchResults);
+            this.groupBox1.Location = new System.Drawing.Point(12, 228);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(500, 312);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Results";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(352, 280);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(108, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export Results";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // dgvSearchResults
+            // 
+            this.dgvSearchResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSearchResults.Location = new System.Drawing.Point(3, 16);
+            this.dgvSearchResults.Name = "dgvSearchResults";
+            this.dgvSearchResults.Size = new System.Drawing.Size(491, 258);
+            this.dgvSearchResults.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSetOutputFilePath);
+            this.groupBox2.Controls.Add(this.chkExportResults);
+            this.groupBox2.Controls.Add(this.lblExportDirectoryLabel);
+            this.groupBox2.Controls.Add(this.lblStats);
+            this.groupBox2.Controls.Add(this.cbDeviceType);
+            this.groupBox2.Controls.Add(this.lblDirectoryPath);
+            this.groupBox2.Controls.Add(this.lbloutputDirectoryPath);
+            this.groupBox2.Controls.Add(this.btnSetDirectoryPath);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Location = new System.Drawing.Point(15, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(497, 210);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log Files Search Criteria";
+            // 
+            // btnSetOutputFilePath
+            // 
+            this.btnSetOutputFilePath.Location = new System.Drawing.Point(137, 115);
+            this.btnSetOutputFilePath.Name = "btnSetOutputFilePath";
+            this.btnSetOutputFilePath.Size = new System.Drawing.Size(139, 23);
+            this.btnSetOutputFilePath.TabIndex = 13;
+            this.btnSetOutputFilePath.Text = "Set Directory Path";
+            this.btnSetOutputFilePath.UseVisualStyleBackColor = true;
+            this.btnSetOutputFilePath.Click += new System.EventHandler(this.btnSetOutputFilePath_Click);
+            // 
+            // chkExportResults
+            // 
+            this.chkExportResults.AutoSize = true;
+            this.chkExportResults.Location = new System.Drawing.Point(137, 92);
+            this.chkExportResults.Name = "chkExportResults";
+            this.chkExportResults.Size = new System.Drawing.Size(131, 17);
+            this.chkExportResults.TabIndex = 12;
+            this.chkExportResults.Text = "Export Search Results";
+            this.chkExportResults.UseVisualStyleBackColor = true;
+            this.chkExportResults.CheckStateChanged += new System.EventHandler(this.chkExportResults_CheckStateChanged);
+            // 
+            // lblExportDirectoryLabel
+            // 
+            this.lblExportDirectoryLabel.AutoSize = true;
+            this.lblExportDirectoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExportDirectoryLabel.Location = new System.Drawing.Point(-1, 120);
+            this.lblExportDirectoryLabel.Name = "lblExportDirectoryLabel";
+            this.lblExportDirectoryLabel.Size = new System.Drawing.Size(134, 13);
+            this.lblExportDirectoryLabel.TabIndex = 11;
+            this.lblExportDirectoryLabel.Text = "Select Exported File path :-";
+            // 
+            // lblStats
+            // 
+            this.lblStats.AutoSize = true;
+            this.lblStats.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblStats.Location = new System.Drawing.Point(330, 73);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(0, 13);
+            this.lblStats.TabIndex = 8;
+            // 
+            // cbDeviceType
+            // 
+            this.cbDeviceType.FormattingEnabled = true;
+            this.cbDeviceType.Items.AddRange(new object[] {
+            "---Select Device Type---",
+            "IPhone",
+            "Android"});
+            this.cbDeviceType.Location = new System.Drawing.Point(137, 65);
+            this.cbDeviceType.Name = "cbDeviceType";
+            this.cbDeviceType.Size = new System.Drawing.Size(174, 21);
+            this.cbDeviceType.TabIndex = 10;
+            // 
+            // lblDirectoryPath
+            // 
+            this.lblDirectoryPath.AutoSize = true;
+            this.lblDirectoryPath.ForeColor = System.Drawing.Color.Green;
+            this.lblDirectoryPath.Location = new System.Drawing.Point(283, 34);
+            this.lblDirectoryPath.Name = "lblDirectoryPath";
+            this.lblDirectoryPath.Size = new System.Drawing.Size(0, 13);
+            this.lblDirectoryPath.TabIndex = 9;
+            this.lblDirectoryPath.DoubleClick += new System.EventHandler(this.lblDirectoryPath_DoubleClick);
+            // 
+            // lbloutputDirectoryPath
+            // 
+            this.lbloutputDirectoryPath.AutoSize = true;
+            this.lbloutputDirectoryPath.ForeColor = System.Drawing.Color.Green;
+            this.lbloutputDirectoryPath.Location = new System.Drawing.Point(134, 141);
+            this.lbloutputDirectoryPath.Name = "lbloutputDirectoryPath";
+            this.lbloutputDirectoryPath.Size = new System.Drawing.Size(0, 13);
+            this.lbloutputDirectoryPath.TabIndex = 9;
+            // 
+            // btnSetDirectoryPath
+            // 
+            this.btnSetDirectoryPath.Location = new System.Drawing.Point(137, 29);
+            this.btnSetDirectoryPath.Name = "btnSetDirectoryPath";
+            this.btnSetDirectoryPath.Size = new System.Drawing.Size(139, 23);
+            this.btnSetDirectoryPath.TabIndex = 8;
+            this.btnSetDirectoryPath.Text = "Set Directory Path";
+            this.btnSetDirectoryPath.UseVisualStyleBackColor = true;
+            this.btnSetDirectoryPath.Click += new System.EventHandler(this.btnSetDirectoryPath_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select log file path :-";
+            // 
+            // frmSearchServiceLogs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(525, 552);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmSearchServiceLogs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search Service Logs";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchResults)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvSearchResults;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSetDirectoryPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDirectoryPath;
+        private System.Windows.Forms.Label lbloutputDirectoryPath;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.ComboBox cbDeviceType;
+        private System.Windows.Forms.Label lblStats;
+        private System.Windows.Forms.Button btnSetOutputFilePath;
+        private System.Windows.Forms.CheckBox chkExportResults;
+        private System.Windows.Forms.Label lblExportDirectoryLabel;
+    }
+}
---
+++ SearchServiceLogs/frmSearchServiceLogs.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SearchServiceLogs
+{
+    public partial class frmSearchServiceLogs : Form
+    {
+        public frmSearchServiceLogs()
+        {
+            _LogfilesDirectory = string.Empty;
+            _ExportfileDirectory = string.Empty;
+            InitializeComponent();
+            cbDeviceType.SelectedIndex = 0;
+            lblExportDirectoryLabel.Visible = lbloutputDirectoryPath.Visible = btnSetOutputFilePath.Visible = false;
+        }
+        public string _LogfilesDirectory { get; set; }
+        public string _ExportfileDirectory { get; set; }
+        DataTable dtSearchResults = null;
+        DataTable dtTempSearchResults = null;
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            if (_LogfilesDirectory != string.Empty && cbDeviceType.SelectedIndex != -1 && cbDeviceType.SelectedIndex != 0)
+            {
+                //Create a New Data table to store search results data per client
+                dtSearchResults = GetTable("Result");
+                dtTempSearchResults = GetTable("TempResult");
+
+                // Get the subdirectories for the specified directory.
+                DirectoryInfo dir = new DirectoryInfo(_LogfilesDirectory);
+                DirectoryInfo[] dirs = dir.GetDirectories("*", SearchOption.TopDirectoryOnly);
+                lblStats.Text = DateTime.Now.ToString("hh:mm:ss");
+                if (dir.Exists)
+                {
+                    foreach (DirectoryInfo subDirectory in dirs)
+                    {
+                        //String List object to store all the Match line for each client
+                        List<string> sbSearchLinePerClient = new List<string>();
+
+                        ////Add Directory Name which would be Client Name to result Data Table
+                        //dtSearchResults.Rows.Add(subDirectory.Name, string.Empty, string.Empty, string.Empty);
+
+                        // Get the files with name "SEDCSmartAppsService_*" in the directory and pull the lines that match the Search text
+                        FileInfo[] files = subDirectory.GetFiles("SEDCSmartAppsService_*", SearchOption.TopDirectoryOnly);
+                        foreach (FileInfo file in files)
+                        {
+                            sbSearchLinePerClient.AddRange(ReturnMatchLines(file.FullName, cbDeviceType.SelectedIndex, string.Empty));
+
+
+                            //Parse all the lines we get for each Client
+                            if (sbSearchLinePerClient.Count > 0)
+                            {
+                                foreach (string line in sbSearchLinePerClient.ToArray())
+                                {
+                                    if (line.IndexOf("<hardwareid>") != -1 && line.IndexOf("</PushNotifyType>") != -1)
+                                    {
+                                        try
+                                        {
+                                            string strFinal = line.Substring(line.IndexOf("<hardwareid>"), line.IndexOf("</PushNotifyType>") - line.IndexOf("<hardwareid>") + 17);
+                                            XmlDocument objXMLDoc = new XmlDocument();
+                                            objXMLDoc.LoadXml("<Data>" + strFinal.Replace("&", "&amp;").Replace("\"", "&quot;").Replace("\'", "&apos;") + "</Data>");
+
+                                            string hardwareId = objXMLDoc.SelectSingleNode("Data/hardwareid").InnerText;
+                                            var foundHardwareID = dtSearchResults.Select("HardwareID = '" + hardwareId + "'");
+                                            if (foundHardwareID.Length == 0)
+                                            {
+                                                //Add IOS Version & Profile Details to result Data Table
+                                                dtSearchResults.Rows.Add(subDirectory.Name, objXMLDoc.SelectSingleNode("Data/profilename").InnerText.Replace("&amp;", "&").Replace("&quot;", "\"").Replace("&apos;", "\'"),
+                                                    objXMLDoc.SelectSingleNode("Data/osversion").InnerText, objXMLDoc.SelectSingleNode("Data/appversion").InnerText, hardwareId);
+                                                dtTempSearchResults.Rows.Add(subDirectory.Name, objXMLDoc.SelectSingleNode("Data/profilename").InnerText.Replace("&amp;", "&").Replace("&quot;", "\"").Replace("&apos;", "\'"),
+                                                    objXMLDoc.SelectSingleNode("Data/osversion").InnerText, objXMLDoc.SelectSingleNode("Data/appversion").InnerText, hardwareId);
+                                            }
+
+                                        }
+                                        catch (Exception Ex)
+                                        {
+                                            MessageBox.Show("There was an error occurred while processing Log Files for client :-" + subDirectory.Name + "  File Name:-" + file.Name, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                        }
+                                    }
+                                }
+                            }
+                            sbSearchLinePerClient.Clear();
+                        }
+                        ////Add a Blank line after Each Client
+                        //dtSearchResults.Rows.Add(string.Empty, string.Empty, string.Empty, string.Empty);
+
+                        //Write to File after per Client reading
+                        if (chkExportResults.Checked && !_ExportfileDirectory.Equals(string.Empty))
+                        {
+                            using (StreamWriter writer = new StreamWriter(_ExportfileDirectory,true))
+                            {
+                                WriteDataTable(dtTempSearchResults, writer, false);
+                                dtTempSearchResults.Clear();
+                            }
+                        }
+                    }
+                    if (dtSearchResults.Rows.Count > 0)
+                    {
+                        dgvSearchResults.DataSource = dtSearchResults;
+                    }
+                    lblStats.Text += " / " + DateTime.Now.ToString("hh:mm:ss");
+                    if (chkExportResults.Checked)
+                        MessageBox.Show("File exported successfully!" + "\r\n"+"File Path:-"+_ExportfileDirectory, "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                if (_LogfilesDirectory == string.Empty)
+                    MessageBox.Show("Please select Directory path for Log Files!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else if (cbDeviceType.SelectedIndex == -1 || cbDeviceType.SelectedIndex == 0)
+                    MessageBox.Show("Please select Device Type!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private string[] ReturnMatchLines(string filePath, int Devicetype, string subString)
+        {
+            //Devicetype=1 for Iphone and Devicetype=2 for Android
+            List<string> matchLines = new List<string>();
+            if (Devicetype == 1)
+            {
+                foreach (var match in File.ReadLines(filePath)
+                                 .Select((text, index) => new { text, lineNumber = index + 1 })
+                                 .Where(x => x.text.Contains("<PushNotifyType>APNS</PushNotifyType>")))
+                {
+                    matchLines.Add(match.text);
+                }
+            }
+            else if (Devicetype == 2)
+            {
+                foreach (var match in File.ReadLines(filePath)
+                                 .Select((text, index) => new { text, lineNumber = index + 1 })
+                                 .Where(x => (x.text.Contains("<PushNotifyType>GCM</PushNotifyType>") || x.text.Contains("<PushNotifyType>CDMA</PushNotifyType>"))))
+                {
+                    matchLines.Add(match.text);
+                }
+            }
+            return matchLines.ToArray();
+        }
+
+        private DataTable GetTable(string tblName)
+        {
+            // Here we create a DataTable with columns.
+            DataTable table = new DataTable(tblName);
+            table.Columns.Add("Client Name", typeof(string));
+            table.Columns.Add("Profile Name", typeof(string));
+            table.Columns.Add("IOS Version", typeof(string));
+            table.Columns.Add("App Version", typeof(string));
+            table.Columns.Add("HardwareID", typeof(string));
+
+            return table;
+        }
+
+        private void btnSetDirectoryPath_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    _LogfilesDirectory = dlg.SelectedPath;
+                    lblDirectoryPath.Text = _LogfilesDirectory;
+                    MessageBox.Show("Directory path for Log Files selected!", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void ExportGrid(DataGridView dGV, string filename)
+        {
+            string stOutput = "";
+            //// Export titles:
+            //string sHeaders = "";
+
+            //for (int j = 0; j < dGV.Columns.Count; j++)
+            //    sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + ",";
+            //stOutput += sHeaders + "\r\n";
+            // Export data.
+            for (int i = 0; i < dGV.RowCount - 1; i++)
+            {
+                string stLine = "";
+                for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                    stLine = stLine.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + ",";
+                stOutput += stLine + "\r\n";
+            }
+            Encoding utf16 = Encoding.GetEncoding(1254);
+            byte[] output = utf16.GetBytes(stOutput);
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(output, 0, output.Length); //write the encoded file
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+        }
+        public static void WriteDataTable(DataTable sourceTable, TextWriter writer, bool includeHeaders)
+        {
+            if (includeHeaders)
+            {
+                List<string> headerValues = new List<string>();
+                foreach (DataColumn column in sourceTable.Columns)
+                {
+                    headerValues.Add(QuoteValue(column.ColumnName));
+                }
+
+                writer.WriteLine(String.Join(",", headerValues.ToArray()));
+            }
+
+            string[] items = null;
+            foreach (DataRow row in sourceTable.Rows)
+            {
+                items = row.ItemArray.Select(o => QuoteValue(o.ToString())).ToArray();
+                writer.WriteLine(String.Join(",", items));
+            }
+
+            writer.Flush();
+        }
+
+        private static string QuoteValue(string value)
+        {
+            return String.Concat("\"", value.Replace("\"", "\"\""), "\"");
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV files (*.csv)|*.csv";
+            sfd.FileName = "export.csv";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                ExportGrid(dgvSearchResults, sfd.FileName);
+                MessageBox.Show("File exported successfully!", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void lblDirectoryPath_DoubleClick(object sender, EventArgs e)
+        {
+            if (!lblStats.Visible)
+                lblStats.Visible = true;
+            else
+                lblStats.Visible = false;
+        }
+
+        private void btnSetOutputFilePath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "CSV files (*.csv)|*.csv";
+                sfd.FileName = "export.csv";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    _ExportfileDirectory = sfd.FileName;
+                    lbloutputDirectoryPath.Text = _ExportfileDirectory;
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+        }
+
+        private void chkExportResults_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (chkExportResults.Checked)
+            {
+                lblExportDirectoryLabel.Visible = lbloutputDirectoryPath.Visible = btnSetOutputFilePath.Visible = true;
+            }
+            else
+            {
+                lblExportDirectoryLabel.Visible = lbloutputDirectoryPath.Visible = btnSetOutputFilePath.Visible = false;
+            }
+        }
+
+
+    }
+}

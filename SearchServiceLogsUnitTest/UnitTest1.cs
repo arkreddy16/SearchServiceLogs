fb29e575a6e91730fb5e012538f552cd4457a49c
ramakrishnaa <ramakrishnaa@proarch.com>
2015-03-10 16:58:39 +0530

    Added Unit Test

---
+++ SearchServiceLogsUnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SearchServiceLogs;
+
+namespace SearchServiceLogsUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(true, false);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            object obj = new object();
+            Assert.AreSame(obj, obj);
+        }
+    }
+}
